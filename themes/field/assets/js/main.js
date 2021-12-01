dbea3cfd1dc025bdd9b859a0974ac8a543cc5931
niazmorshad <78914574+niazmorshad@users.noreply.github.com>
2021-12-01 20:16:40 +0600

    ..

--- themes/field/assets/js/main.js
+++ themes/field/assets/js/main.js
@@ -474,7 +474,48 @@ if( $('.category-box-cntlr').length ){
         ]
       });
   }
-
+ if (windowWidth <= 767) {
+  if( $('.teamSlider').length ){
+      $('.teamSlider').slick({
+        dots: true,
+        arrows:false,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        speed: 700,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: true
+            }
+          }
+        ]
+      });
+  }
+}
 
 
 new WOW().init();
